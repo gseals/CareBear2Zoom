ce9eeaff57eeacdbf1058338088a6a714837da8b
Gabriel Seals <sealsgj@gmail.com>
2020-04-15 15:43:58 -0500

    start at 2:07:00

--- CareBear2Zoom/Program.cs
+++ CareBear2Zoom/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using CareBear2Zoom.Bears;
@@ -41,7 +42,47 @@ namespace CareBear2Zoom
 
             // transforming the List. similar to map in JS. not removing things, but changing what the collection is made of
             // bear.Name tells Select that we will be returning an IEnumerable of strings
+            // IEnumerable are the lowest common denominator for all of our connections
+            // as long as it is an ienumerable, you can use linq on it
             var careBearNames = careBears.Select(bear => bear.Name);
+
+            // be aware of the order in which you do things because it effects your outcome
+            // this order takes the original list and filters it down to a smaller subset before filtering the smaller group down more
+            var careBearNamesThatStartWithT = careBears.Where(bear =>bear.Name.StartsWith('T')).Select(bear => bear.Name);
+
+            // lambda expressions (delegates): the fat arrow occuring inside of parentheses on linq methods; it is a function reference
+            // the is operator allows us to name something specifically and it returns a boolean expression representing the result
+            // Any returns true if any one of the thing in the list matches
+            // Any: one thing must match; as soon as it finds the first of the thing, it stops looking through the list
+            var anyTinderHearts = careBears.Any(bear => bear is TinderHeart);
+
+            // All returns true if every bear in our list if a TinderHeart
+            // All: looks until it finds something that doesn't match and then exits out - short circuiting
+            var allTinderHearts = careBears.All(bear => bear is TinderHeart);
+
+            // when you put in your dot operator, anything with a disc and a down arrow are the linq things
+            // aggragate is the same concept as JS reduce
+
+            // first says find me the first thing in the list and return it
+            var firstBear = careBears.First();
+
+            // entering a parameter gives the method something look for;
+            // if you ask for something that doesn't exist, it blows up - throws an exception
+            var firstBearStartingWithT = careBears.First(bear => bear.Name.StartsWith("Te"));
+
+            // if you create a list with nothing in it
+            var otherList = new List<CareBearBase>();
+            // if you ask for the first thing in a list that doesn't have anything in it, it blows up - throw an exception
+            var blowsUp = otherList.First();
+
+            // FirstOrDefault says find the first thing that matches, or give me the default of carebearbase
+            // a reference type, like a class, default to null
+            var works = careBears.FirstOrDefault(bear => bear.Name.StartsWith('W'));
+
+            // skip one item and then take 2 items; throws an exception if used on a list of not three things
+            var skippedGrumpy= careBears.Skip(1).Take(2);
+
+
 
         }
     }
